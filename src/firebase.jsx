6c6fc61f17538b61c057b75050bd0972dc02368d
makryo <mpachecoa2@miumg.edu.gt>
2022-07-26 16:47:32 -0600

    install firebase

---
+++ src/firebase.jsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAn-PVhq61wng8VfCjQ3ke9sYju-JX2UYc",
+    authDomain: "mariotodo-1214a.firebaseapp.com",
+    projectId: "mariotodo-1214a",
+    storageBucket: "mariotodo-1214a.appspot.com",
+    messagingSenderId: "918538614508",
+    appId: "1:918538614508:web:b7091fa9f4337cd30f57c9",
+    measurementId: "G-TF0DERHXW1"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
